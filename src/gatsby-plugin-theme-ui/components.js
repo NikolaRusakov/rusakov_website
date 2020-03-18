6b8060811d9ad9987be971c028fedc9173316cea
nikolarusakov <nikola.rusakov@rusakov.cz>
2020-03-18 23:42:35 +0100

    fix(typography-design-tools): not really fix, let's rewrite DesignTool Yay
    - flow sucks :(
    - rewrite of [DesignTool](https://github.com/KyleAMathews/typography.js/blob/master/packages/typography-design-tools/src/DesignTool.js) is waiting

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -4,10 +4,12 @@ import React from 'react';
 import Codeblock from './codeblock';
 import wrapTypography from 'typography-design-tools';
 import githubTheme from 'typography-theme-github';
+import Typography from 'typography';
 
+const typography = new Typography(githubTheme);
 export default {
   pre: props => props.children,
-  DesignTool: wrapTypography(githubTheme),
+  DesignTool: wrapTypography(typography),
   code: Codeblock /*{
     // return <ThemeUIPrism {...props} Prism={PrismCore} copy={true}/>;
   },*/,
