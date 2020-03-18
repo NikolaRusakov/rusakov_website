ecfe089e05826a987f78c816b7dd7a8ee9231027
nikolarusakov <nikola.rusakov@rusakov.cz>
2020-03-18 21:18:45 +0100

    feat(typography): add typography.js tooling

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -2,9 +2,12 @@ import React from 'react';
 // import ThemeUIPrism from '@theme-ui/prism';
 // import PrismCore from 'prismjs/components/prism-core';
 import Codeblock from './codeblock';
+import wrapTypography from 'typography-design-tools';
+import githubTheme from 'typography-theme-github';
 
 export default {
   pre: props => props.children,
+  DesignTool: wrapTypography(githubTheme),
   code: Codeblock /*{
     // return <ThemeUIPrism {...props} Prism={PrismCore} copy={true}/>;
   },*/,
