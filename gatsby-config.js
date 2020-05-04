cd633a99634e82feb03b88a7651ecef181281ee5
nikolarusakov <nikola.rusakov@rusakov.cz>
2020-05-05 01:59:06 +0200

    feat(rw/github-source): sourcing from Github

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,12 @@ const fetch = require('node-fetch');
 const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
 console.log(`Using environment config: '${activeEnv}'`);
-require('dotenv').config({
-  path: `.env.${activeEnv}`,
-});
+
+if (activeEnv !== 'production') {
+  require('dotenv').config({
+    path: `.env.${activeEnv}`,
+  });
+}
 
 module.exports = {
   siteMetadata: {
